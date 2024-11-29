b8a7b04bb66fc09ccf096d0597bbaf8023b7e6c3
Rakesh Biswas <rbiswas01999@gmail.com>
2024-11-29 19:46:09 +0600

    basic structure added

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,32 @@
-import React, { FC } from 'react';
-import './App.css';
+import React, { ChangeEvent, FC, useState } from "react";
+import "./App.css";
 
-const App:FC = () =>  {
+const App: FC = () => {
+
+  const [task, setTask] = useState<string>("")
+  const [deadline, setDeadline] = useState<number>(0)
+  const [todo, setTodo] = useState([])
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    if(event.target.value === 'task'){
+      setTask(event.target.value);
+    }else{
+      setDeadline(Number(event.target.value))
+    }
+  }
+
   return (
     <div className="App">
-      <div className='header'>
-        <input type="text" placeholder='Task...' />
-        <input type="number" placeholder='Deadline (in Days)...' />
+      <div className="header">
+        <div className="inputContainer">
+          <input type="text" placeholder="Task..." name="task" onChange={handleChange} />
+          <input type="number" placeholder="Deadline (in Days)..." name="deadline" />
+        </div>
         <button>Add Task</button>
       </div>
-      <div className='todoList'></div>
+      <div className="todoList"></div>
     </div>
   );
-}
+};
 
 export default App;
